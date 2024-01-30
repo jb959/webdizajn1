3b80d552b5d9ade20a8c03ea37ecf196f402085d
jb959 <156956135+jb959@users.noreply.github.com>
2024-01-30 19:42:58 +0100

    Add files via upload

---
+++ js/copyme.js
@@ -0,0 +1,9 @@
+
+function skopiruj(){
+  /*takto to pojde najlepsie, nech to skopiruje do stranky, naco by to malo hladat
+  funkcia skpiruje do schranky tento text
+  */
+    
+   navigator.clipboard.writeText("+421 949 123 456")
+
+}
